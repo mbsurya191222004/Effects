4e9730319a1a71fed57e7396b2c387a7cd7722f6
Surya <mbsurya19122004@gmail.com>
2025-07-30 18:40:57 +0530

    addded menu v2

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,23 @@
+import {motion} from "motion/react"
+
 function Menu() {
     return (  
         
-        <div className="bg-amber-900 fixed h-[30%] w-[40%] right-0 flex flex-col justify-evenly">
+        <motion.div 
+        initial = {{
+            opacity : 0 ,
+            top : "-100%"
+        }}
+        animate = {{
+                opacity : 1 ,
+                top : "7vh",
+            }}
+        transition={{
+            duration : 1 ,
+        }}
+        className="bg-amber-900 fixed h-[30%] w-[40%] right-0 flex flex-col justify-evenly z-[-1]"
+        >
+
             <a href="/home" className="bg-amber-200 h-[30%] text-lg flex flex-col justify-evenly items-center ">
                 Home
             </a>
@@ -11,7 +27,7 @@ function Menu() {
             <a  href="/contact" className="bg-amber-200 h-[30%] text-lg flex flex-col justify-evenly items-center">
                 Contact
             </a>
-        </div>
+        </motion.div>
         
     );
 }
